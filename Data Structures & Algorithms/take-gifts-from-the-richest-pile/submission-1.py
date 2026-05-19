1bbe3793129b865f0b9b0331d4c4efafb5337203
Ziwei Liu <55415786+ZiweiLiu1314@users.noreply.github.com>
2026-05-19 20:25:56 +0800

    Add: take-gifts-from-the-richest-pile - submission-1

---
+++ Data Structures & Algorithms/take-gifts-from-the-richest-pile/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        gifts = [-n for n in gifts]
+        for _ in range(k): 
+            heapq.heapify(gifts)
+            top = -heapq.heappop(gifts)
+            top = int(sqrt(top))
+            heapq.heappush(gifts, -top)
+        return -sum(gifts) 
+
+        # time complexity: O(n + k log n) 
+        # space complexity: O(n)
