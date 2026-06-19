9b7e5b21d2dbd5abad86f3ad212866b8b03c44c3
Ziwei Liu <55415786+ZiweiLiu1314@users.noreply.github.com>
2026-06-19 10:27:06 +0800

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # sort characters in strings, 
+        # so that they become identical if they're anagrams 
+        # sorted characters: a key 
+        # append the original string to the list corresponding to this key 
+        # in the hashmap 
+        res = defaultdict(list)
+        for s in strs: 
+            key = "".join(sorted(s))
+            res[key].append(s)
+        return list(res.values())
+
