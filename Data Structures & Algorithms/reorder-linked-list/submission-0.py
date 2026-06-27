5a8527c4f90f44e14267f676733db64032c619ca
Ziwei Liu <55415786+ZiweiLiu1314@users.noreply.github.com>
2026-06-27 14:37:53 +0800

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        # reorder the nodes in a singly linked list, to [0, n-1, 1, n-2, 2, n-3, ...]
+        # brute force: traverse through the list to find out the length n 
+        # travers through the linked list to find each value and put them in the right place, starting from the node 0 
+        # O(n^2) time 
+        # optimized: do one go and insert the nodes at the right places 
+        # determine the length of the list in one go, and as we do so we build a hash map val2node O(n)
+        # construct the new linked list using the hash map, O(n)
+        # O(n) time 
+        n = 0 
+        idx2node = defaultdict(ListNode)
+        node = head
+        while node:
+            idx2node[n] = node 
+            n = n + 1 
+            print(node.val)
+            node = node.next 
+        print(f"n = {n}")
+        node = head
+        for i in range(1, n):
+            if i % 2 == 0: # even idx 
+                node.next = idx2node[i//2]
+                print(f"the idx of the next node to node: {node.val} is {i}")
+            else: 
+                node.next = idx2node[n - (i // 2) - 1] 
+                print(f"the idx of the next node to node: {node.val} is {i}")
+            node = node.next 
+        node.next = None 
